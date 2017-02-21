ee377defb8fdd77c0d82d1dfc6ac12c941d4fe46
Kowalczuk <ekowalczuk@positivo.com.br>
2017-02-21 11:31:34 -0300

    Criada Suite de teste

---
+++ app/src/androidTest/java/positivo/qa_automation/AllTests.java
@@ -0,0 +1,32 @@
+package positivo.qa_automation;
+
+/**
+ * Created by Kowalczuk on 21/02/2017.
+ */
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+import android.test.suitebuilder.TestSuiteBuilder;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+
+@Suite.SuiteClasses({
+        CalculatorTest.class,
+        AlarmTest.class,
+        CameraTest.class,
+        GoogleTest.class,
+        ContactsTest.class,
+        HomeTest.class,
+        PropertiesTest.class
+
+})
+
+public class AllTests  extends TestSuite{
+
+    public static Test suite(){
+        return new TestSuiteBuilder(AllTests.class).includeAllPackagesUnderHere().build();
+    }
+}
